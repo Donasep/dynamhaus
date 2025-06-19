8ea6f8053594f13865d9c2383653a72e7a792850
donasep <184644367+Donasep@users.noreply.github.com>
2025-06-20 01:57:23 +0200

    j

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_log("INDEX.PHP: Entrée. REQUEST_URI: " . ($_SERVER['REQUEST_URI'] ?? 'Non défini'));
+error_log("INDEX.PHP: \$_GET contenu: " . print_r($_GET, true));
+
 require __DIR__."/config/autoload.php";
 use App\Lib\Router\Router;
 session_start();
