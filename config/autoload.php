9cd30385789dd80096e794828715d3fb59207969
donasep <184644367+Donasep@users.noreply.github.com>
2025-06-20 00:23:40 +0200

    j

--- config/autoload.php
+++ config/autoload.php
@@ -3,7 +3,7 @@ spl_autoload_register(function (string $class) {
 
     $namespaceParts = explode('\\', $class);
 
-    $filepath = '/' . implode('/',$namespaceParts) . ".php";
+    $filepath = dirname(__DIR__). '/' . implode('/',$namespaceParts) . ".php";
     if (!file_exists($filepath)) {
         throw new Exception("Fichier ".$filepath." est introuvable pour la classe ".$class);
     }
