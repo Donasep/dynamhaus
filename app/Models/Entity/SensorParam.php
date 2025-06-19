38e065fdc855301d3720cf381d610fefea2d835e
donasep <184644367+Donasep@users.noreply.github.com>
2025-06-20 00:13:37 +0200

    destroy

--- app/Models/Entity/SensorParam.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models\Entity;
-class SensorParam {
-    public ?int $sensorId;
-    public ?float $samplingDelay;
-    public ?bool $active;
-    const TABLE_NAME = 'sensorParam';
-}
